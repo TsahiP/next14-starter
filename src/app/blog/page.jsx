b8828de6a59519693fe1632204297e187c2ff9c2
tsahi pahima <tsahi013@gmail.com>
2024-06-27 01:11:44 +0300

    .

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,7 +5,7 @@ import styles from "./blog.module.css";
 
 const getData = async () => {
   
-  const res = await fetch("http://localhost:3000/api/blog",{next: {revalidate:3600}});
+  const res = await fetch("https://tpnext-six.vercel.app/api/blog",{next: {revalidate:3600}});
 
   if (!res.ok) {
     throw new Error("Something went wrong");
